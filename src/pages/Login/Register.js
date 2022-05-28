d3145a6e47259b66f008a9716027a65938706a12
ARIFULISLAMSAGOR <arifulislamsagor86@gmail.com>
2022-05-28 17:22:01 +0600

    my orders added

--- src/pages/Login/Register.js
+++ src/pages/Login/Register.js
@@ -3,6 +3,7 @@ import { useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfil
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import useToken from '../hooks/useToken';
 import Loading from '../Shared/Loading';
 
 const Register = () => {
@@ -16,6 +17,8 @@ const Register = () => {
     ] = useCreateUserWithEmailAndPassword(auth);
 
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const [token] = useToken(user || gUser);
 
     const navigate = useNavigate();
 
@@ -32,12 +35,14 @@ const Register = () => {
     if (user || gUser) {
         console.log(user || gUser);
     }
-
+    if (token) {
+        navigate('/');
+    }
     const onSubmit = async data => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         console.log('update done');
-        navigate('/');
+        // navigate('/');
     }
     return (
         <div>
